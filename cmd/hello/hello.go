20dd9c7e97c756e85c1ae24cd5b3c82b100ad526
Georgy <gstarikov@gmail.com>
2018-12-08 11:53:42 +0300

    interface added

--- cmd/hello/hello.go
+++ cmd/hello/hello.go
@@ -5,11 +5,47 @@ import (
 	"os"
 )
 
-func main() {
+// Greeter greets someone by name
+type Greeter interface {
+	Greet(name string)
+}
 
-	fmt.Printf("cmd line total args count is %d. Wow!\n",len(os.Args))
-	for i, v := range os.Args {
-		fmt.Printf("\targ[%d] -> %s\n",i,v)
+type PrefixGreeter struct {
+
+}
+
+func (p PrefixGreeter) Greet(name string) {
+	fmt.Println("hello",name)
+}
+
+type SuffixGreeter struct {
+
+}
+
+func (p SuffixGreeter) Greet(name string) {
+	fmt.Println(name,"hello")
+}
+
+
+func main() {
+	var op, name string
+
+	if len (os.Args)>=2{
+		op = os.Args[1]
 	}
-	fmt.Println("hello world")
+	if len(os.Args)>=3 {
+		op = os.Args[2]
+	}
+	var g Greeter
+	switch op {
+	case "prefix":
+		g = PrefixGreeter{}
+	case "suffix":
+		g = SuffixGreeter{}
+	default:
+		fmt.Printf("unexpected greeter[%s]\n",op)
+		return
+	}
+
+	g.Greet(name)
 }
