34aac351500b545f49e310d17be0f6845428469f
Georgy <gstarikov@gmail.com>
2018-12-08 16:26:19 +0300

    refactor

--- cmd/filegen/main.go
+++ cmd/filegen/main.go
@@ -25,14 +25,14 @@ func main() {
 
 	rand.Seed(time.Now().UnixNano())
 
-	res := make(chan error,n)
+	res := make(chan error, n)
 
 	for i := 0; i < n; i++ {
-		go fileWriter(res,i,m)
+		go fileWriter(res, i, m)
 	}
 	for i := 0; i < n; i++ {
 		if err := <-res; err != nil {
-			log.Printf("file [%d], err -> %s",i,err)
+			log.Printf("file [%d], err -> %s", i, err)
 		}
 	}
 }
@@ -52,23 +52,16 @@ func fileWriter(res chan<- error, i, m int) {
 		return
 	}
 	defer file.Close()
-	err = writeRandNumbersToFile(file)
-	if err != nil {
-		return
-	}
-
-	for j := 0; j < m; j++ {
-		if err = writeRandNumbersToFile(file); err != nil {
-			return
-		}
-	}
-	res <- nil
+	err = writeRandNumbersToFile(file, m)
+	res <- err
 }
 
-func writeRandNumbersToFile(file *os.File) error {
-	bts := randNumberBytes()
-	if _, err := file.Write(bts); err != nil {
-		return err
+func writeRandNumbersToFile(file *os.File, m int) error {
+	for j := 0; j < m; j++ {
+		bts := randNumberBytes()
+		if _, err := file.Write(bts); err != nil {
+			return err
+		}
 	}
 	return nil
 }
